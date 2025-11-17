cb47f92a8fcbf34bc5ba4ea47bf132cb6159932b
Kirill Grafov <brauergetz@mail.ru>
2025-11-17 20:54:06 +0300

    All done

--- homework/lab3/4.py
+++ homework/lab3/4.py
@@ -1,15 +1,3 @@
-# Этот способ не подходит
-# Надо всё переделать
-
-
-
-
-
-
-
-
-
-
 # 4. Поиск максимальной суммы
 # подмассива фиксированной длины
 #
@@ -23,32 +11,27 @@
 
 arr = [1, -2, 3, 4, -1, 2, 1, -5, 4]
 newArr = []
+nestedList = []
 k = 3
 
-#nested_list = []
-#for i in range(0, len(arr), k):
-#  nested_list.append(arr[i:i+1])
-#print(nested_list)
+x = len(arr)
 
-
-#for i in range(len(arr) // k):
-#    x = []
-#    for j in range(k):
-#        x.append(arr[i * k + j])
-#    newArr.append(x)
+for i in range (0, len(arr)):
+    if x < 3:
+        break
+    else:
+        newArr += (arr[i], arr[i + 1], arr[i + 2])
+        x -= 1
 
 #print(newArr)
 
+for j in range(0, len(newArr), k):
+    nestedList.append(newArr[j:j+k])
 
-#current_sum = sum(arr[:k])
-#max_sum = current_sum
+#print(nestedList)
 
-#for i in range(k, len(arr)):
-#    current_sum = current_sum - arr[i-k] + arr[i]
-#    if max_sum < current_sum:
-#        max_sum = current_sum
+max_list = max(nestedList, key = sum)
+max_sum = sum(max(nestedList, key=sum))
 
-
-#print(max_sum)
-
-
+print(arr)
+print("Подмассив ", max_list, "Сумма ", max_sum)
